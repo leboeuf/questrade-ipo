e0b1616c7a0fb74f89c49e1625ab6a28cc52b3ca
mleboeuf <ml@maximeleboeuf.com>
2017-10-31 22:17:41 -0400

    Get symbol from listing

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using AngleSharp.Parser.Html;
 using Newtonsoft.Json;
 
 namespace questrade_ipo
@@ -21,13 +23,21 @@ namespace questrade_ipo
         public string Status { get; set; }
     }
 
-    public class Ipo
+    public class Ipo : IpoListItem
     {
-
+        public object[] Documents { get; set; }
+        public string Jurisdiction { get; set; }
+        public string MetaDescription { get; set; }
+        public string MetaKeywords { get; set; }
+        public string MinimumSubscription { get; set; }
+        public string Pricing { get; set; }
+        public string RegisteredPlanEligibility { get; set; }
+        public string SummeryDetails { get; set; }
     }
 
     class Program
     {
+        private static HtmlParser _htmlParser = new HtmlParser();
         private static HttpClient _httpClient = new HttpClient();
         
         static async Task Main(string[] args)
@@ -47,8 +57,18 @@ namespace questrade_ipo
                 // var ipoJson = await _httpClient.GetStringAsync($"http://www.questrade.com/Sitefinity/Public/Services/Questrade/Services/IPOService.svc/GetIPODetails/{ipo.IpoURL}/");
                 // await File.WriteAllTextAsync($"/Users/node/Desktop/ipos/{ipo.IpoURL}.json", ipoJson);
                 // Console.WriteLine($"{i}/{ipos.Count - 1} ({ipo.IpoURL})");
-                var ipoJson = await File.ReadAllTextAsync("/Users/node/Desktop/ipos/{ipo.IpoURL}.json");
+                var ipoJson = await File.ReadAllTextAsync($"/Users/node/Desktop/ipos/{ipo.IpoURL}.json");
+                var ipoItem = JsonConvert.DeserializeObject<Ipo>(ipoJson);
 
+                var regex = new Regex("under the symbol “(.*)”.", RegexOptions.IgnoreCase);
+                var matches = regex.Matches(ipoItem.SummeryDetails);
+                if (!matches.Any())
+                {
+                    continue;
+                }
+
+                var symbol = matches[0].Groups[1].Value;
+                Console.WriteLine($"{ipoItem.DateAdded}\t{symbol}\t{ipoItem.Industry}\t{ipoItem.IssuerName}");
             }
         }
     }

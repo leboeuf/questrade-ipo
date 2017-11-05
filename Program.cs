38d694c8b2e07e0a138d6725fd35330dc91d2dff
mleboeuf <ml@maximeleboeuf.com>
2017-11-05 14:56:50 -0500

    Get historical data from API and calculate profit or loss

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AngleSharp.Parser.Html;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace questrade_ipo
 {
@@ -37,11 +38,19 @@ namespace questrade_ipo
 
     class Program
     {
+        private const string APIURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&apikey={0}&datatype=csv&symbol=TSX:{1}";
+        private const string APIKEY = ""; // Put your API key here
         private static HtmlParser _htmlParser = new HtmlParser();
         private static HttpClient _httpClient = new HttpClient();
         
         static async Task Main(string[] args)
         {
+            if (APIKEY == string.Empty)
+            {
+                Console.WriteLine("You must set your API key. Aborting.");
+                return;
+            }
+
             // var json = await _httpClient.GetStringAsync("http://www.questrade.com/Sitefinity/Public/Services/Questrade/Services/IPOService.svc/GetIPOList");
             // await File.WriteAllTextAsync("/Users/node/Desktop/ipos.json", json);
             var json = await File.ReadAllTextAsync("/Users/node/Desktop/ipos.json");
@@ -68,7 +77,42 @@ namespace questrade_ipo
                 }
 
                 var symbol = matches[0].Groups[1].Value;
-                Console.WriteLine($"{ipoItem.DateAdded}\t{symbol}\t{ipoItem.Industry}\t{ipoItem.IssuerName}");
+                if (symbol.Length > 6)
+                {
+                    // Quick fix for unclean matches that grab more than just the symbol because of special characters
+                    continue;
+                }
+
+                var apiResponse = await _httpClient.GetStringAsync(string.Format(APIURL, APIKEY, symbol));
+                if (apiResponse.Contains("Error"))
+                {
+                    // If first character is \n there was an error (API has no data for this symbol)
+                    continue;
+                }
+
+                var allLines = apiResponse.Split("\r\n")
+                    .Skip(1) // Remove the header row
+                    .Reverse().Skip(1).Reverse(); // Remove the last (empty) element
+                var parsedCsv = allLines.Take(1) // Newest data point
+                .Union(allLines.Reverse().Take(1)) // Oldest data point
+                .Select(line => line.Split(','))
+                .Select(csv => new
+                {
+                    Date = csv[0],
+                    Open = double.Parse(csv[1]),
+                    High = double.Parse(csv[2]),
+                    Low = double.Parse(csv[3]),
+                    Close = double.Parse(csv[5]), // Adjusted close
+                    Volume = int.Parse(csv[6]),
+                }).ToList();
+
+                if (parsedCsv.Count > 2)
+                {
+                    continue;
+                }
+
+                var profit = Math.Round(parsedCsv[1].Close - parsedCsv[0].Close, 2);
+                Console.WriteLine($"{symbol}: {profit}");
             }
         }
     }
--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # Questrade IPO
 
-The program gets the list of all IPO's from the IPO Centre and then loops through the list to find those of type "Equity" that are closed. It then fetches the IPO data of each IPO and keeps only those that have a stock symbol in them. Then it downloads the historical stock data from the date of the IPO until now and does various calculations to determine whether it would have been profitable to invest in the IPO's.
+The program gets the list of all IPO's on the TSX from the IPO Centre and then loops through the list to find those of type "Equity" that are closed. It then fetches the IPO data of each IPO and keeps only those that have a stock symbol in them. Then it downloads the historical stock data from the date of the IPO until now and does various calculations to determine whether it would have been profitable to invest in the IPO's.
+
+It uses the Alpha Vantage API to get historical data. Alpha Vantage doesn't support specifying date ranges so the values are based on the last 100 days of data.
+
+## Results
+
+Example output (profit per share):
+
+```
+RRX: 0.42
+AAV: 1.04
+PVG: -1.87
+ENB: 4.3
+ECI: -0.58
+STN: -5.02
+H: 0.79
+MTL: -0.8
+SBB: -0.4
+FRU: -2.71
+CNL: 0.34
+ERF: -1.1
+TOY: -10.97
+KEY: 4.85
+FCR: 0.26
+GEI: 0.42
+ALA: 0.85
+BNP: -0.04
+SU: -4.11
+DPM: -0.18
+TMR: 6.95
+CG: -0.54
+BSX: 0.12
+BXE: 1.17
+FCR: 0.26
+IPL: -0.33
+X: -2.17
+CPG: 1.43
+NVA: -1.71
+```
